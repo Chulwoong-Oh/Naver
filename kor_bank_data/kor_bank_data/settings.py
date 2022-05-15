8f31dbd4eafd14867860077cb0cba11b6d79c19a
unknown <feboy00@gmail.com>
2022-05-15 12:19:17 +0900

    update 05/16

--- kor_bank_data/kor_bank_data/settings.py
+++ kor_bank_data/kor_bank_data/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'kor_bank_data.spiders'
 #USER_AGENT = 'kor_bank_data (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
